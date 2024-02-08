ebd8ff8044ca46be3464ee13207f8df8b0c18fd3
livia araujo <liviaaraujo.dev@gmail.com>
2024-02-08 16:26:53 -0300

    create projetcts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "@/components/About";
 import { Header } from "@/components/Header/";
 import { Home } from "@/components/Home";
+import { Projects } from "@/components/Projects";
 import { Technologies } from "@/components/Technologies";
 import Image from "next/image";
 
@@ -12,6 +13,7 @@ export default function App() {
         <Home />
         <About />
         <Technologies />
+        <Projects />
       </main>
     </>
   );
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,26 +1,31 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdOutlineWhatsapp } from "react-icons/md";
 
-export function Footer(){
-    return(
-        <footer>
-               <p className="text-purple-100 text-lg font-normal mt-4 lg:w-[80%]">
-          I am a seasoned full-stack software engineer with over 8 years of
-          professional experience, specializing in backend development. 8 years
-          of professional experience, specializing in backend development.{" "}
-        </p> 
+export function Footer() {
+  return (
+    <footer>
+      <div className="md:text-center ml-8" >
+        <p>
+          <span className="text-[#BA00EF] text-lg">© Lívia Araujo 2023</span>
+          <p>
+            Todos os
+            direitos reservados
+          </p>
+        </p>
+      </div>
 
-        <div className="flex gap-4 mt-4">
-          <a href="https://github.com/liviaaraujo-dev" target="_blank">
-            <FaGithub size={35} color="white" />
-          </a>
-          <a href="https://www.linkedin.com/in/liviaaraujodev" target="_blank">
-            <FaLinkedin size={35} color="white" />
-          </a>
-          <a href="https://wa.me/558892858695" target="_blank">
-            <MdOutlineWhatsapp size={40} color="white" />
-          </a>
-        </div> 
-        </footer>
-    )
+      <div className="flex gap-4 mt-4">
+        <a href="https://github.com/liviaaraujo-dev" target="_blank">
+          <FaGithub size={35} color="white" />
+        </a>
+        <a href="https://www.linkedin.com/in/liviaaraujodev" target="_blank">
+          <FaLinkedin size={35} color="white" />
+        </a>
+        <a href="https://wa.me/558892858695" target="_blank">
+          <MdOutlineWhatsapp size={40} color="white" />
+        </a>
+      </div>
+    </footer>
+  )
 }
+
